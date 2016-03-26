4a1a5414902d5f0061200df20d62348fd958f06a
Jingxiang Li <lijingxiang.bill@gmail.com>
2016-03-26 00:58:03 -0500

    prototype for model training

--- train_models.py
+++ train_models.py
@@ -1,10 +1,11 @@
 from __future__ import division, absolute_import
 from __future__ import print_function, unicode_literals
-import xgboost as xg
+import xgboost as xgb
 import numpy as np
 import pandas as pd
 from os import listdir
-
+from sklearn.cross_validation import StratifiedShuffleSplit
+from sklearn.metrics import f1_score
 
 def aggregate_features(X):
     return np.mean(X, axis=0)
@@ -41,8 +42,63 @@ def get_response():
     return response_frame
 
 
-X = get_features()
-Y = get_response()
-dataframe = pd.merge(X, Y, left_index=True, right_index=True)
+def get_data():
+    X = get_features()
+    Y = get_response()
+    dataframe = pd.merge(X, Y, left_index=True, right_index=True)
+    return dataframe
 
 
+dataframe = get_data().as_matrix()
+X = dataframe[:, 0:1024]
+y = dataframe[:, -1].astype(int)
+print(np.sum(y == 1) / y.shape[0])
+dtrain = xgb.DMatrix(X, label=y)
+
+param = {'max_depth':1, 'eta':1, 'silent':1, 'objective':'binary:logitraw', 'lambda':1}
+param['nthread'] = 4
+
+# watchlist  = [(dtrain,'train'), (dtest,'eval')]
+num_round = 2000
+
+def evalerror(preds, dtrain):
+    pred_labels = [1 if p > 0 else 0 for p in preds]
+    labels = dtrain.get_label()
+    return 'f1-score', f1_score(labels, pred_labels)
+
+def fpreproc(dtrain, dtest, param):
+    label = dtrain.get_label()
+    ratio = float(np.sum(label == 0)) / np.sum(label==1)
+    param['scale_pos_weight'] = ratio
+    return (dtrain, dtest, param)
+
+xgb.cv(param, dtrain, num_round, feval=evalerror, early_stopping_rounds=30, maximize=True, verbose_eval=True, fpreproc=fpreproc)
+
+
+
+
+
+
+
+
+
+# sss = StratifiedShuffleSplit(dataframe[:, -3], 1, test_size=0.2)
+# for train_index, test_index in sss:
+#     X_train, X_test = X[train_index], X[test_index]
+#     y_train, y_test = y[train_index], y[test_index]
+
+# dtrain = xgb.DMatrix(X_train, label=y_train)
+# dtest = xgb.DMatrix(X_test, label=y_test)
+# param = {'max_depth':3, 'eta':0.1, 'silent':1, 'objective':'binary:logitraw', 'lambda':1}
+# param['nthread'] = 4
+
+# watchlist  = [(dtrain,'train'), (dtest,'eval')]
+# num_round = 2000
+
+
+# def evalerror(preds, dtrain):
+#     pred_labels = [1 if p > 0 else 0 for p in preds]
+#     labels = dtrain.get_label()
+#     return 'f1-score', f1_score(labels, pred_labels)
+
+# bst = xgb.train(param, dtrain, num_round, watchlist, feval=evalerror, early_stopping_rounds=30, maximize=True)
