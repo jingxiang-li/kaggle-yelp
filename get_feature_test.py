18c88ae9003f5a3ecf1b6c7fce5cb4b823ee50b4
Jingxiang Li <lijingxiang.bill@gmail.com>
2016-04-08 14:08:01 -0500

    compatible with inception v3 features

--- get_feature_test.py
+++ get_feature_test.py
@@ -5,40 +5,55 @@ from __future__ import unicode_literals
 
 import numpy as np
 import pandas as pd
+from sys import argv
+import os
+from os import path
 
-prob = 0.75
-reps = 5
 
-# process test data
-img_df = pd.read_csv("data/imglist_test.txt", sep="\t", header=None)
-img_index = img_df.ix[:, 0].as_matrix()
-feature_21k = np.load("feature/inception-21k-test.npy")
-feature_colHist = np.load("feature/colHist-test.npy")
+def main(prob=0.75, reps=5):
 
-img_feature_df = pd.DataFrame(
-    data=np.hstack((feature_21k, feature_colHist)),
-    index=img_index)
+    # process test data
+    img_df = pd.read_csv("data/imglist_test.txt", sep="\t", header=None)
+    img_index = img_df.ix[:, 0].as_matrix()
+    feature_21k = np.load("feature/inception-21k-test.npy")
+    feature_colHist = np.load("feature/colHist-test.npy")
+    feature_v3 = np.load("feature/inception-v3-test.npy")
 
-photo2biz = pd.read_csv(
-    "data/test_photo_to_biz.csv", index_col='photo_id')
-biz_list = photo2biz["business_id"].unique()
+    img_feature_df = pd.DataFrame(
+        data=np.hstack((feature_21k, feature_colHist, feature_v3)),
+        index=img_index)
 
-print(biz_list.shape)
-result_list = []
+    photo2biz = pd.read_csv(
+        "data/test_photo_to_biz.csv", index_col='photo_id')
+    biz_list = photo2biz["business_id"].unique()
 
-for c, biz_id in enumerate(biz_list):
-    photo_ids = photo2biz[photo2biz["business_id"] == biz_id].index.values
-    length = len(photo_ids)
-    for i in range(reps):
-        sample = np.random.choice(
-            photo_ids, size=int(prob * length), replace=False)
-        fset = img_feature_df.loc[sample, :]
-        f_all = np.hstack(
-            (np.mean(fset, axis=0), np.std(fset, axis=0)))
-        result_list.append(f_all)
-    if c % 100 == 0:
-        print(c)
+    print(biz_list.shape)
+    result_list = []
 
-X_test = np.asarray(result_list)
-np.save("feature/mean_5_075/X_test.npy", X_test)
-np.save("feature/mean_5_075/bizlist.npy", np.asarray(biz_list))
+    for c, biz_id in enumerate(biz_list):
+        photo_ids = photo2biz[photo2biz["business_id"] == biz_id].index.values
+        length = len(photo_ids)
+        for i in range(reps):
+            sample = np.random.choice(
+                photo_ids, size=int(prob * length), replace=False)
+            fset = img_feature_df.loc[sample, :]
+            f_all = np.hstack(
+                (np.mean(fset, axis=0), np.std(fset, axis=0)))
+            result_list.append(f_all)
+        if c % 100 == 0:
+            print(c)
+
+    X_test = np.asarray(result_list)
+
+    dir_path = "feature/" + str(reps) + "_" + str(int(100 * prob))
+    print(dir_path)
+    if not path.isdir(dir_path):
+        os.mkdir(dir_path)
+    np.save(path.join(dir_path, "X_test.npy"), X_test)
+    np.save(path.join(dir_path, "bizlist.npy"), np.asarray(biz_list))
+
+
+if __name__ == "__main__":
+    assert(len(argv) == 3)
+    prob, reps = float(argv[1]), int(argv[2])
+    main(prob, reps)
--- get_features.py
+++ get_features.py
@@ -5,6 +5,9 @@ from __future__ import unicode_literals
 
 import numpy as np
 import pandas as pd
+from sys import argv
+import os
+from os import path
 
 
 def parse_labels(s, n=9):
@@ -14,44 +17,55 @@ def parse_labels(s, n=9):
             v[int(x)] = 1
     return v
 
-prob = 0.75
-reps = 5
 
-# Process Training Data
-img_df = pd.read_csv("data/imglist_train.txt", sep="\t", header=None)
-img_index = img_df.ix[:, 0].as_matrix()
-feature_21k = np.load("feature/inception-21k-train.npy")
-feature_colHist = np.load("feature/colHist-train.npy")
+def main(prob=0.75, reps=5):
 
-img_feature_df = pd.DataFrame(
-    data=np.hstack((feature_21k, feature_colHist)),
-    index=img_index)
+    # Process Training Data
+    img_df = pd.read_csv("data/imglist_train.txt", sep="\t", header=None)
+    img_index = img_df.ix[:, 0].as_matrix()
+    feature_21k = np.load("feature/inception-21k-train.npy")
+    feature_colHist = np.load("feature/colHist-train.npy")
+    feature_v3 = np.load("feature/inception-v3-train.npy")
 
-photo2biz = pd.read_csv(
-    "data/train_photo_to_biz_ids.csv", index_col='photo_id')
-biz_list = photo2biz["business_id"].unique()
+    img_feature_df = pd.DataFrame(
+        data=np.hstack((feature_21k, feature_colHist, feature_v3)),
+        index=img_index)
 
-result_list = []
+    photo2biz = pd.read_csv(
+        "data/train_photo_to_biz_ids.csv", index_col='photo_id')
+    biz_list = photo2biz["business_id"].unique()
 
-for biz_id in biz_list:
-    photo_ids = photo2biz[photo2biz["business_id"] == biz_id].index.values
-    length = len(photo_ids)
-    for i in range(reps):
-        sample = np.random.choice(
-            photo_ids, size=int(prob * length), replace=False)
-        fset = img_feature_df.loc[sample, :]
-        f_all = np.hstack(
-            (np.mean(fset, axis=0), np.std(fset, axis=0)))
-        result_list.append(f_all)
+    result_list = []
 
-X_train = np.asarray(result_list)
+    for biz_id in biz_list:
+        photo_ids = photo2biz[photo2biz["business_id"] == biz_id].index.values
+        length = len(photo_ids)
+        for i in range(reps):
+            sample = np.random.choice(
+                photo_ids, size=int(prob * length), replace=False)
+            fset = img_feature_df.loc[sample, :]
+            f_all = np.hstack(
+                (np.mean(fset, axis=0), np.std(fset, axis=0)))
+            result_list.append(f_all)
 
-biz2label = pd.read_csv(
-    "data/train.csv", index_col='business_id')
-y_train = np.zeros((X_train.shape[0], 9), dtype="int8")
-for i, biz_id in enumerate(biz_list):
-    y_train[range(5 * i, 5 * i + 5), :] = \
-        parse_labels(biz2label.loc[biz_id]["labels"])
+    X_train = np.asarray(result_list)
 
-np.save("feature/mean_5_075/X_train.npy", X_train)
-np.save("feature/mean_5_075/y_train.npy", y_train)
+    biz2label = pd.read_csv(
+        "data/train.csv", index_col='business_id')
+    y_train = np.zeros((X_train.shape[0], 9), dtype="int8")
+    for i, biz_id in enumerate(biz_list):
+        y_train[range(5 * i, 5 * i + 5), :] = \
+            parse_labels(biz2label.loc[biz_id]["labels"])
+
+    dir_path = "feature/" + str(reps) + "_" + str(int(100 * prob))
+    print(dir_path)
+    if not path.isdir(dir_path):
+        os.mkdir(dir_path)
+    np.save(path.join(dir_path, "X_train.npy"), X_train)
+    np.save(path.join(dir_path, "y_train.npy"), y_train)
+
+
+if __name__ == "__main__":
+    assert(len(argv) == 3)
+    prob, reps = float(argv[1]), int(argv[2])
+    main(prob, reps)
